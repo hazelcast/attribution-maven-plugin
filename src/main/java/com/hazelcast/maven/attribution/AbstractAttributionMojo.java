17274e48ed8432ca3bf1435b28d7069d847e0923
Jack Green <JackPGreen@Gmail.com>
2025-04-22 17:28:50 +0100

    Move Maven to Sisu, not Plexus DI framework

--- src/main/java/com/hazelcast/maven/attribution/AbstractAttributionMojo.java
+++ src/main/java/com/hazelcast/maven/attribution/AbstractAttributionMojo.java
@@ -7,7 +7,6 @@ import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.DefaultProjectBuildingRequest;
 import org.apache.maven.project.MavenProject;
@@ -44,6 +43,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import javax.inject.Inject;
 
 public abstract class AbstractAttributionMojo extends AbstractMojo {
 
@@ -126,7 +126,7 @@ public abstract class AbstractAttributionMojo extends AbstractMojo {
     @Parameter
     protected List<String> exclusionPatterns;
 
-    @Component
+    @Inject
     protected AttribResourceResolver resourceResolver;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
